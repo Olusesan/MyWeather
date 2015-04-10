6a7e22a362ee9e934502ab66c361e533c02ce88f
Olusesan <olusesan.ajina@gmail.com>
2015-04-10 12:02:46 -0400

    Initial Commit

---
+++ MyWeather/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyWeather
+//
+//  Created by Olusesan Ajina on 4/10/15.
+//  Copyright (c) 2015 Olusesan Ajina. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
